3c968d3b999de00e8613ee37ad3e430ca8c818b9
Karthik Arumugam <chivalrykarthik@gmail.com>
2018-12-23 17:44:54 +0530

    Set path to Logo

--- src/components/Header.js
+++ src/components/Header.js
@@ -5,7 +5,7 @@ function Header(props) {
     return (
         <div className="col-sm-12">
             <nav className="navbar navbar-expand-lg navbar-light justify-content-between">
-                <a className="navbar-brand" href="localhost">GitSearch</a>
+                <a className="navbar-brand" href="/">GitSearch</a>
                 <button className="btn btn-info pull-left">Compare</button>
             </nav>
         </div>);
