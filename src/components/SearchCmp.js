8ec31c59f22abe7e28b952b8548eff85aa7ea29f
Karthik Arumugam <chivalrykarthik@gmail.com>
2018-12-28 21:13:13 +0530

    Added new search property topic

--- src/components/SearchCmp.js
+++ src/components/SearchCmp.js
@@ -2,19 +2,29 @@ import React from 'react';
 import axios from '../axiosApi.js';
 
 
+function InputComponent(props) {
+    return (
+        <div className={props.className} >
+            <input type="text" className="form-control" placeholder={props.placeholder} name={props.name} value={props.value} onChange={props.onChange} />
+        </div>);
+}
+
 class SearchCmp extends React.Component {
     state = {};
     constructor(props) {
         super(props);
         this.state.query = '';
         this.state.language = '';
+        this.state.topic = '';
         this.state.searchQuery = '';
         this.state.searchLanguage = '';
+        this.state.searchTopic = '';
         this.state.searchInProgress = false;
         this.state.newSearch = true;
         this.searchGitHub = this.searchGitHub.bind(this);
         this.handleChange = this.handleChange.bind(this);
         this.handleSearch = this.handleSearch.bind(this);
+        this.prepareSearchURL = this.prepareSearchURL.bind(this);
     }
 
     handleChange(e) {
@@ -26,38 +36,54 @@ class SearchCmp extends React.Component {
     }
     handleSearch() {
         if (this.state.searchInProgress) return;
-        if (!this.state.query || !this.state.language) {
+        if (!this.state.query && !this.state.language  && !this.state.topic) {
             this.setState({ "searchInProgress": false });
             return alert("Please enter valid data to search");
         }
         this.props.setPage(1);
-        this.setState({ searchQuery: this.state.query, searchLanguage: this.state.language,newSearch:true }, () => {            
+        this.setState({ "searchQuery": this.state.query, "searchLanguage": this.state.language,"searchTopic": this.state.topic, "newSearch": true }, () => {
             this.searchGitHub(this.props.pageConf.page);
-        })
+        });
+    }
+
+    prepareSearchURL(pageNo) {
+        let searchURL = `search/repositories?q=`;
+        if (this.state.searchQuery) {
+            searchURL += this.state.searchQuery;
+        }
+        if (this.state.searchLanguage) {
+            searchURL += `+language:${this.state.searchLanguage}`;
+        }
+        if (this.state.searchTopic) {
+            searchURL += `+topic:${this.state.searchTopic}`;
+        }
+        searchURL += `&sort=stars&order=desc&per_page=${this.props.pageConf.per_page}&page=${pageNo}`;
+        return searchURL;
     }
     searchGitHub(pageNo) {
         if (this.state.searchInProgress) return;
         this.setState({ "searchInProgress": true });
-        if (!this.state.searchQuery || !this.state.searchLanguage) {
+        if (!this.state.searchQuery && !this.state.searchLanguage && !this.state.searchTopic) {
             this.setState({ "searchInProgress": false });
             return alert("Please enter valid data to search");
         }
-        const searchUrl = `search/repositories?q=${this.state.searchQuery}+language:${this.state.searchLanguage}&sort=stars&order=desc&per_page=${this.props.pageConf.per_page}&page=${pageNo}`;
-        axios.get(searchUrl)
+        const searchURL = this.prepareSearchURL(pageNo);
+
+        axios.get(searchURL)
             .then(res => {
                 this.props.setRecords(res);
-                this.setState({ "searchInProgress": false,newSearch:false });
+                this.setState({ "searchInProgress": false, newSearch: false });
             })
             .catch(e => {
                 console.log(e.stack);
-                this.setState({ "searchInProgress": false,newSearch:false });
+                this.setState({ "searchInProgress": false, newSearch: false });
                 alert("We are not getting valid response from Github. Please try again later");
             });
     }
 
-    componentDidUpdate(prevProps,prevState) {
-        if(prevProps.pageConf.page !== this.props.pageConf.page && !this.state.newSearch){
-            this.searchGitHub(this.props.pageConf.page);            
+    componentDidUpdate(prevProps, prevState) {
+        if (prevProps.pageConf.page !== this.props.pageConf.page && !this.state.newSearch) {
+            this.searchGitHub(this.props.pageConf.page);
         }
 
     }
@@ -68,15 +94,12 @@ class SearchCmp extends React.Component {
                 <div className="col-sm-12">
                     <form>
                         <div className="form-group row">
-                            <div className="col-sm-3">
-                                <input type="text" className="form-control" placeholder="Keyword" name='query' value={this.state.query} onChange={this.handleChange} />
-                            </div>
-                            <div className="col-sm-3">
-                                <input type="text" className="form-control" placeholder="language" name="language" value={this.state.language} onChange={this.handleChange} />
-                            </div>
-                            <div className="col-sm-4 d-none">
-                                <input type="text" className="form-control" placeholder="Or Repo URL" />
-                            </div>
+                            <InputComponent className="col-sm-3" placeholder="Keyword" name='query' value={this.state.query} onChange={this.handleChange} />
+
+                            <InputComponent className="col-sm-3" placeholder="Language" name="language" value={this.state.language} onChange={this.handleChange} />
+
+                            <InputComponent className="col-sm-3" placeholder="Topic" name="topic" value={this.state.topic} onChange={this.handleChange} />
+
                             <div className="col-sm-2">
                                 <button type="button" className={"btn btn-success float-left" + (this.state.searchInProgress ? " disabled" : "")} onClick={this.handleSearch}>Search</button>
                             </div>
